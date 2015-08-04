06a9e44f57e3de4b1120565acd0b69ff3bfdda42
David Conner <dconner.pro@gmail.com>
2015-08-04 12:50:27 -0400

    changed angular app name name to appistack

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('appistack.controllers', [])
   .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
     // With the new view caching in Ionic, Controllers are only called
